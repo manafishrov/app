cc314965fe150a53a6d50fb5b22e1605bc9a34ef
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-06-24 17:40:27 +0200

    feat: add model for settings

---
+++ src-tauri/src/models/settings.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Settings {
+  pub pitch_stabalizon: bool,
+  pub roll_stabalizon: bool,
+  pub depth_stabalizon: bool,
+}
+
+impl Default for Settings {
+  fn default() -> Self {
+    Settings {
+      pitch_stabalizon: true,
+      roll_stabalizon: true,
+      depth_stabalizon: true,
+    }
+  }
+}
