80431d305047362f285ac3688d1e34ad1f270602
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-07-12 16:25:28 +0200

    feat: add spinner to general ROV config page

--- src/routes/settings/general/index.tsx
+++ src/routes/settings/general/index.tsx
@@ -12,12 +12,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/Select';
+import { Spinner } from '@/components/ui/Spinner';
 import { toast } from '@/components/ui/Toaster';
 
 import { useFirmwareVersionListener } from '@/hooks/useFirmwareVersionListener';
 
 import { logError } from '@/lib/log';
 
+import { connectionStatusStore } from '@/stores/connectionStatus';
 import { rovConfigStore, setRovConfig } from '@/stores/rovConfig';
 
 async function requestFirmwareVersion() {
@@ -35,6 +37,10 @@ export const Route = createFileRoute('/settings/general/')({
 });
 
 function General() {
+  const isConnected = useStore(
+    connectionStatusStore,
+    (state) => state.isConnected,
+  );
   const rovConfig = useStore(rovConfigStore, (state) =>
     state
       ? {
@@ -54,46 +60,54 @@ function General() {
           Generic settings for the Manafish ROV.
         </p>
       </div>
-      <div className='space-y-6'>
-        {firmwareVersion && (
+      {!isConnected || !rovConfig ? (
+        <div className='flex h-96 w-full items-center justify-center'>
+          <Spinner size='lg' />
+        </div>
+      ) : (
+        <div className='space-y-6'>
+          {firmwareVersion && (
+            <div>
+              <h4 className='text-lg font-medium'>Firmware Version</h4>
+              <p className='text-muted-foreground text-sm'>
+                Current version of the Manafish ROV firmware.
+              </p>
+              <Badge className='bg-primary/10 text-primary mt-2 rounded-full px-3 py-1 text-sm font-medium'>
+                v{firmwareVersion}
+              </Badge>
+            </div>
+          )}
           <div>
-            <h4 className='text-lg font-medium'>Firmware Version</h4>
+            <h4 className='text-lg font-medium'>Fluid type</h4>
             <p className='text-muted-foreground text-sm'>
-              Current version of the Manafish ROV firmware.
+              Set correctfluidtype to get accurate pressure and temperature
+              readings.
             </p>
-            <Badge className='bg-primary/10 text-primary mt-2 rounded-full px-3 py-1 text-sm font-medium'>
-              v{firmwareVersion}
-            </Badge>
-          </div>
-        )}
-        <div>
-          <h4 className='text-lg font-medium'>Fluid type</h4>
-          <p className='text-muted-foreground text-sm'>
-            Set correctfluidtype to get accurate pressure and temperature
-            readings.
-          </p>
-          <div className='mt-2 flex items-center gap-3'>
-            <Select
-              value={rovConfig.fluidType}
-              onValueChange={(value) =>
-                setRovConfig({ fluidType: value as 'freshwater' | 'saltwater' })
-              }
-              disabled={!rovConfig}
-            >
-              <SelectTrigger className='w-40'>
-                <SelectValue placeholder='Select fluid type' />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Fluid Type</SelectLabel>
-                  <SelectItem value='freshwater'>Freshwater</SelectItem>
-                  <SelectItem value='saltwater'>Saltwater</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            <div className='mt-2 flex items-center gap-3'>
+              <Select
+                value={rovConfig.fluidType}
+                onValueChange={(value) =>
+                  setRovConfig({
+                    fluidType: value as 'freshwater' | 'saltwater',
+                  })
+                }
+                disabled={!rovConfig}
+              >
+                <SelectTrigger className='w-40'>
+                  <SelectValue placeholder='Select fluid type' />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fluid Type</SelectLabel>
+                    <SelectItem value='freshwater'>Freshwater</SelectItem>
+                    <SelectItem value='saltwater'>Saltwater</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
