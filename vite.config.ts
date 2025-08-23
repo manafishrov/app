1d2b46c1a47136e7e015f4f080b06397c3b5cc58
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-08-23 03:24:07 +0200

    feat: upgrade to react swc vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
