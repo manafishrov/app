907afd17057fab58fc73b457e902bc96a4afdf7e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-03 00:09:00 +0100

    feat: try to optimize rpm overlay

--- src/components/overlay/ThrusterRpmOverlay.tsx
+++ src/components/overlay/ThrusterRpmOverlay.tsx
@@ -6,10 +6,24 @@ import { configStore } from '@/stores/config';
 import { connectionStatusStore } from '@/stores/connectionStatus';
 import { rovTelemetryStore } from '@/stores/rovTelemetry';
 
-function ThrusterRpmOverlay() {
-  const thrusterRpms = useStore(
+function ThrusterItem({ index }: { index: number }) {
+  const rpm = useStore(
     rovTelemetryStore,
-    (state) => state.thrusterRpms,
+    (state) => state.thrusterRpms[index]!,
+  );
+
+  return (
+    <div className='flex items-center gap-1 text-xs'>
+      <ThrusterRpm rpm={rpm} />
+      <span className='w-2'>T{index + 1}</span>
+    </div>
+  );
+}
+
+function ThrusterRpmOverlay() {
+  const thrusterRpmsLength = useStore(
+    rovTelemetryStore,
+    (state) => state.thrusterRpms.length,
   );
   const isConnected = useStore(
     connectionStatusStore,
@@ -24,14 +38,8 @@ function ThrusterRpmOverlay() {
 
   return (
     <div className='flex flex-col items-end gap-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
-      {thrusterRpms.map((rpm, index) => (
-        <div
-          key={`thruster-${index + 1}`}
-          className='flex items-center gap-1 text-xs'
-        >
-          <ThrusterRpm rpm={rpm} />
-          <span className='w-2'>T{index + 1}</span>
-        </div>
+      {Array.from({ length: thrusterRpmsLength }, (_, i) => i).map((index) => (
+        <ThrusterItem key={`thruster-${index + 1}`} index={index} />
       ))}
     </div>
   );
