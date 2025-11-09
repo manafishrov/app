8a84584d0cd3f80cd32553ed17de2f784ee1a44d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-09 11:19:40 +0100

    Revert "chore: revert build changes"
    This reverts commit 1f5ee45b6c3ff3f50303f4943b15d2b68fbd2573.

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,5 +5,7 @@ fn main() {
   #[cfg(target_os = "linux")]
   println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
+  std::env::set_var("FFMPEG_CONFIGURE_FLAGS", "--disable-avfoundation");
+
   tauri_build::build()
 }
