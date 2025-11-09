010d340f8b0e5cd9b61e206381e7db46921a0fa9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-09 23:59:48 +0100

    chore: try linking binaries

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,7 +5,5 @@ fn main() {
   #[cfg(target_os = "linux")]
   println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 
-  std::env::set_var("FFMPEG_CONFIGURE_FLAGS", "--disable-avfoundation");
-
   tauri_build::build()
 }
