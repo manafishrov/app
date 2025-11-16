dedd32275a525476db98d6531de02b2205034e5c
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-16 15:46:48 +0100

    fix: add ffmpeg to proper dll search ppath using windows-sys

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,19 +2,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[cfg(target_os = "windows")]
-fn add_ffmpeg_resources_to_path() {
-  use std::{env, path::PathBuf};
+fn add_ffmpeg_resources_to_dll_search_path() {
+  use std::{env, ffi::OsStr, os::windows::ffi::OsStrExt, path::PathBuf};
+  use windows_sys::Win32::System::LibraryLoader::{
+    AddDllDirectory, SetDefaultDllDirectories, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS,
+  };
 
   if let Ok(exe_path) = env::current_exe() {
     if let Some(exe_dir) = exe_path.parent() {
       let resources_dir: PathBuf = exe_dir.join("resources");
+
       if resources_dir.is_dir() {
-        let mut new_path = resources_dir.into_os_string();
-        if let Some(old_path) = env::var_os("PATH") {
-          new_path.push(";");
-          new_path.push(old_path);
+        if let Some(path_str) = resources_dir.to_str() {
+          let wide: Vec<u16> = OsStr::new(path_str)
+            .encode_wide()
+            .chain(std::iter::once(0))
+            .collect();
+
+          unsafe {
+            SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+            AddDllDirectory(wide.as_ptr());
+          }
         }
-        env::set_var("PATH", new_path);
       }
     }
   }
@@ -22,7 +31,7 @@ fn add_ffmpeg_resources_to_path() {
 
 fn main() {
   #[cfg(target_os = "windows")]
-  add_ffmpeg_resources_to_path();
+  add_ffmpeg_resources_to_dll_search_path();
 
   manafish_lib::run()
 }
