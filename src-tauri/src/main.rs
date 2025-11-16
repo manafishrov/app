0bf678517c997618e23c7b68aa2bf402fc453018
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-16 15:19:46 +0100

    feat: add resorces to path to make ffmpeg available

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "windows")]
+fn add_ffmpeg_resources_to_path() {
+  use std::{env, path::PathBuf};
+
+  if let Ok(exe_path) = env::current_exe() {
+    if let Some(exe_dir) = exe_path.parent() {
+      let resources_dir: PathBuf = exe_dir.join("resources");
+      if resources_dir.is_dir() {
+        let mut new_path = resources_dir.into_os_string();
+        if let Some(old_path) = env::var_os("PATH") {
+          new_path.push(";");
+          new_path.push(old_path);
+        }
+        env::set_var("PATH", new_path);
+      }
+    }
+  }
+}
+
 fn main() {
+  #[cfg(target_os = "windows")]
+  add_ffmpeg_resources_to_path();
+
   manafish_lib::run()
 }
