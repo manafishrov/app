6ff330c5bef8b7c4414554c007181ffbd3541a4f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-08 20:53:45 +0000

    feat: add nix dev flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      rustToolchain = fenix.packages.${system}.latest.withComponents [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+        "rust-analyzer"
+      ];
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustToolchain
+          cmake
+          pkg-config
+          SDL2
+          SDL2.dev
+          bun
+        ];
+      };
+    });
+  };
+}
