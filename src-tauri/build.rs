e965405cf0cf5f3f138b383039baeea4454adaeb
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-09 22:37:14 +0000

    chore: use frameworks for external libs on macos

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-  #[cfg(target_os = "macos")]
-  println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../Resources");
-
-  #[cfg(target_os = "linux")]
-  println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../lib/Manafish");
-
   tauri_build::build()
 }
