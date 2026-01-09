092bf4d602a2e7b3071caaf4b683457d3a27b883
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-09 23:17:04 +0000

    fix: putting libraries in root instead of libs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+  #[cfg(target_os = "macos")]
+  println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../Frameworks");
+
+  #[cfg(target_os = "linux")]
+  println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../lib/Manafish");
+
   tauri_build::build()
 }
