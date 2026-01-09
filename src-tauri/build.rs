b3a0b6b41194f9c7798731f1141a2857c6971aff
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-09 16:19:07 +0000

    chore: revert back to bundled sdl2

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+  if cfg!(target_os = "macos") {
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+  }
+  if cfg!(target_os = "linux") {
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+  }
   tauri_build::build()
 }
