9d30b84df42b5bfb414c7fdc829edbdb518c4849
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-20 17:49:20 +0200

    fix: linting

--- src-tauri/src/commands/rov_config.rs
+++ src-tauri/src/commands/rov_config.rs
@@ -3,9 +3,9 @@ use tauri::{State, command};
 use crate::models::rov_config::{McuBoard, PartialRovConfig, ThrusterTest};
 use crate::websocket::client::MessageSendChannelState;
 use crate::websocket::send::config::{
-  handle_cancel_regulator_auto_tuning, handle_cancel_thruster_test,
-  handle_flash_mcu_firmware, handle_request_rov_config, handle_set_rov_config,
-  handle_start_regulator_auto_tuning, handle_start_thruster_test,
+  handle_cancel_regulator_auto_tuning, handle_cancel_thruster_test, handle_flash_mcu_firmware,
+  handle_request_rov_config, handle_set_rov_config, handle_start_regulator_auto_tuning,
+  handle_start_thruster_test,
 };
 
 #[command]
